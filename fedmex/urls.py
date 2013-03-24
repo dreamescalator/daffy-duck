35930037806900ad10582d21c8ddd0810eb8b55c
Luis Murillo <lmurillo@plos.org>
2013-03-23 19:35:57 -0700

    Update urls.py
    This comment was not supposed to be there.

--- fedmex/urls.py
+++ fedmex/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-   # url(r'^ship/', include('ship.urls')),
+    url(r'^ship/', include('ship.urls')),
     url(r'^admin/', include(admin.site.urls))
 )
