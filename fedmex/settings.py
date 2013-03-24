0f2089678f7a4b87afd0ad1690fa96cfe1686317
Luis Murillo <lmurillo@plos.org>
2013-03-23 19:40:30 -0700

    Update settings.py
    Updated db params to play nice with Heroku

--- fedmex/settings.py
+++ fedmex/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 # Django settings for fedmex project.
 
 DEBUG = True
@@ -9,8 +10,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
+DATABASES = {'default' :  dj_database_url.config()}
+    '''    
+        {
         'ENGINE': 'django.db.backends.sqlite3',# Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'fedmex_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
@@ -19,7 +21,10 @@ DATABASES = {
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
+    
 }
+'''
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
